1774add8beba9e6e567caf0ea4f65c50b14fbdf2
Kailas Moon <kailasmoon@gmail.com>
2018-10-24 20:17:47 -0400

    Deploying Vue stff to heroku

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const serveStatic = require('serve-static')
 const PORT = process.env.PORT || 5000
 
 express()
@@ -8,4 +9,6 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/login', (req, res) => res.render('pages/login'))
+  .get('/vue', (req, res) => res.sendFile(__dirname + '/yna/dist/index.html'))
+  .use(serveStatic(__dirname + "/yna/dist"))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
